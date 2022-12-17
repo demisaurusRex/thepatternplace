e7b38f8bedc0369ddae9864cdddb66e77bdcc218
Stephen Choo <stephen.w.choo@gmail.com>
2022-12-17 11:21:34 +1100

    Added image, sizing, fabric_usage, pdf attachments to pattern model

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -3,6 +3,10 @@ class Pattern < ApplicationRecord
   validates :difficulty,
             acceptance:
             { accept: ["easy", "medium", "hard"] }
+  has_one_attached :image
+  has_one_attached :sizing
+  has_one_attached :fabric_usage
+  has_one_attached :pdf
   has_many :orders
   has_many :instructions
 end
