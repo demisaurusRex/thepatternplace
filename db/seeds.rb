5f7483b02b0c0d1da8443b0d58d6009b6df88f84
Stephen Choo <stephen.w.choo@gmail.com>
2022-12-17 16:55:19 +1100

    Added a show page - layout and styling is still rough and likely to change. Added an image carousel, seeded a pattern and some images. Carousel code can likely be partially reused for the instructions page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+
+print `rails db:drop`
+puts "Database resetted"
+puts "Creating database"
+print `rails db:create`
+puts "Databse created"
+print `rails db:migrate`
+
+puts "Creating users..."
+User.create(email: "stephen@test.com", password: "123456")
+User.create(email: "demi@test.com", password: "123456")
+User.create(email: "yatin@test.com", password: "123456")
+User.create(email: "pizza@pasta.com", password: "123456")
+puts 'Users created'
+
+
+seed_images = [
+  {
+    images: [
+      "#{Rails.root}/seedimages/seed1_image1.jpg",
+      "#{Rails.root}/seedimages/seed1_image2.jpg",
+      "#{Rails.root}/seedimages/seed1_image3.jpg"
+    ],
+    fabric: "#{Rails.root}/seedimages/seed1_fabric.png",
+    sizing: "#{Rails.root}/seedimages/seed1_sizing.png"
+  }
+]
+
+Pattern.create(
+  title: "Vogue Misses Special Occasion Dress",
+  description: "Dress is fitted through bust and hip with lined body, unlined sleeve and flounce, purchased bias tape to finish armhole edge, narrow hems and length and trim variations. B: Purchased trim.",
+  difficulty: "easy",
+  price: 10,
+  user_id: 1
+)
+
+seed_images.each_with_index do |seed_image, index|
+  pattern = Pattern.find(index + 1)
+  seed_image[:images].each_with_index do |image, image_index|
+    pattern.image.attach(
+      io: File.open(image),
+      filename: "image_#{index + 1}#{image_index}.jpg",
+      content_type: "image/jpg"
+    )
+  end
+  pattern.fabric_usage.attach(
+    io: File.open(seed_image[:fabric]),
+    filename: "fabric_#{index + 1}.png",
+    content_type: "image/png")
+  pattern.sizing.attach(
+    io: File.open(seed_image[:sizing]),
+    filename: "sizing_#{index + 1}.png",
+    content_type: "image/png"
+  )
+end
+
+
+# seed 1 image http://res.cloudinary.com/dhnpqzuip/image/upload/v1671241308/dwhoyitivawract6dsih.jpg
+# seed 1 fabric http://res.cloudinary.com/dhnpqzuip/image/upload/v1671241371/fhqrxqxf3wvtlfyhhuj4.png
+# seed 1 sizing http://res.cloudinary.com/dhnpqzuip/image/upload/v1671241390/uyhs2h8jqlql5scowedm.png
