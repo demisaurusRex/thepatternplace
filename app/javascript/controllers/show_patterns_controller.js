7ef944577063cf310e893c3ba274381f6e5aa274
Stephen Choo <stephen.w.choo@gmail.com>
2023-01-03 20:18:55 +1100

    Partially completed show/hide functionality

--- app/javascript/controllers/show_patterns_controller.js
+++ app/javascript/controllers/show_patterns_controller.js
@@ -2,6 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-patterns"
 export default class extends Controller {
+  static targets = ["sizing", "fabric", "fabricbutton"]
   connect() {
+    console.log("loaded")
+  }
+
+  showSizing() {
+    // const booking = document.getElementById(`booking-${bookingId}`)
+    this.sizingTarget.classList.toggle("hidden")
+    console.log("clicked")
+  }
+
+  showFabric() {
+    this.fabricbuttonTarget.classList.toggle("border-bottom-0")
+    this.fabricTarget.classList.toggle("hidden")
+    console.log("clicked")
   }
 }
