267bd5afed10be22a9054ce8737abcae009a5106
Stephen Choo <stephen.w.choo@gmail.com>
2023-01-05 19:25:41 +1100

    Basic shopping cart functionality complete. Altering the frontend now to match the current Figma workup

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,27 @@
+class LineItemsController < ApplicationController
+  def create
+    # Find associated product and current cart
+    chosen_pattern = Pattern.find(params[:pattern_id])
+    current_cart = @current_cart
+    @line_item = LineItem.new
+    @line_item.cart = current_cart
+    @line_item.pattern = chosen_pattern
+    @line_item.user = current_user
+
+    # Save and redirect to cart show path
+    @line_item.save
+    redirect_to cart_path(current_cart)
+  end
+
+  def destroy
+    @line_item = LineItem.find(params[:id])
+    @line_item.destroy
+    redirect_to cart_path(@current_cart)
+  end
+
+  private
+
+  def line_item_params
+    params.require(:line_item).permit(:pattern_id, :cart_id, :user_id)
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
   belongs_to :pattern
   has_many :reviews
   has_many :purchased_instructions
+
+  validates :user, uniqueness: { scope: :pattern }
 end
