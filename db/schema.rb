97dca416699bf957d880192aaf42d9b7be493ed5
Stephen Choo <stephen.w.choo@gmail.com>
2023-01-21 10:33:05 +1100

    Fixed glitch with routes duplication

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_085739) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_01_17_100054) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
